05c1758a2682ae952c2bddd4902ec696182a876e
Yifei Yao <56761074+securityCFS@users.noreply.github.com>
2026-08-05 17:31:34 +0800

    Update GitHub Pages for the InternRobotics repository

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/gauge-web/",
+  base: "/GAUGE/",
   root: "github-pages",
   publicDir: "../public",
   plugins: [react()],
